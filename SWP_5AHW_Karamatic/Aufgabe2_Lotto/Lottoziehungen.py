36d7140e4bca86078f932f3a93af11ce69b08920
i_ate_ur_couch <96168952+iAteYourCouch@users.noreply.github.com>
2024-10-02 22:45:28 +0200

    Update Lottoziehungen.py

--- SWP_5AHW_Karamatic/Aufgabe2_Lotto/Lottoziehungen.py
+++ SWP_5AHW_Karamatic/Aufgabe2_Lotto/Lottoziehungen.py
@@ -1,2 +1,22 @@
-a = list(range(45))
-print(a)
+import random
+
+pBalls = 6
+history = {}
+for i in range(1, 46):
+    history[i] = 0
+
+def lotto(li):
+    drawn_numbers = []
+    for z in range(pBalls):
+        num = random.randint(0, len(li) - 1)
+        drawn_numbers.append(li[num])
+        history[li[num]] += 1
+        del li[num]
+    return drawn_numbers
+
+for i in range(100):
+    numbers = list(range(1, 46))
+    result = lotto(numbers)
+    print(f" {i + 1}: {sorted(result)}")
+
+print(history)
